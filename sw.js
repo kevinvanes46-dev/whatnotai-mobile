49b929b39d3b3d9aee75d73afa6252eec81ed884
kevinvanes46-dev <kevinvanes46@gmail.com>
2026-08-06 18:07:50 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,11 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(keys => Promise.all(
+        keys
+          .filter(key => key.startsWith('whatnotai-mobile') || key.startsWith('cardscout'))
+          .map(key => caches.delete(key))
+      ))
       .then(() => self.registration.unregister())
       .then(() => self.clients.claim())
   );
